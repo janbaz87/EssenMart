38fcc14620de2ceed5714b1e8d95bcd21466e261
Janbaz Ali <janbaz.ali@getir.com>
2024-08-21 12:25:36 +0500

    Create Project

---
+++ EssenMart/EssenMart/EssenMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssenMartApp.swift
+//  EssenMart
+//
+//  Created by Janbaz Ali on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EssenMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
