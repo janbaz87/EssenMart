09f27837af32ec97643561e1d92add4699ec61af
Janbaz Ali <janbaz.ali@getir.com>
2024-08-24 15:14:44 +0500

    Add HTTPMethod enum

---
+++ EssenMart/Networking/HTTPMethod/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Networking
+//
+//  Created by Janbaz Ali on 23/08/2024.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
